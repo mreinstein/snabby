c1515e59db0f12bc2f7b8b39259e5ab5efbba9ae
Mike <reinstein.mike@gmail.com>
2021-03-29 19:39:44 -0700

    point at temporary hyperx module

--- create.js
+++ create.js
@@ -1,5 +1,5 @@
 import { init, thunk, h } from 'snabbdom'
-import hyperx             from 'hyperx'
+import hyperx             from 'hyperx-tmp'
 
 
 export default function create (modules, options={}) {
