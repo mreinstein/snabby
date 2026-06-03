ebc43762c107bb90d5310be4cfc3a5be9e39b2f3
Michael <reinstein.mike@gmail.com>
2026-06-02 21:34:05 -0700

    update deps

---
+++ test/setup.js
@@ -0,0 +1,6 @@
+import { Window } from 'happy-dom'
+
+const window = new Window()
+
+global.window = window
+global.document = window.document
--- test/test.js
+++ test/test.js
@@ -1,23 +1,18 @@
-import snabby from '../index.js'
-import { h }  from 'snabbdom' // helper function for creating vnodes
-import test   from 'tape'
-import { Window } from 'happy-dom'
+import './setup.js'
+import snabby   from '../index.js'
+import { h }    from 'snabbdom' // helper function for creating vnodes
+import assert   from 'node:assert/strict'
+import test     from 'node:test'
 
 
-global.window = new Window()
-global.document = window.document
-
-
-test('creation', function (t) {
-  t.plan(2)
-
-  t.same(
+test('creation', function () {
+  assert.deepEqual(
     snabby`<span>Hello world!</span>`,
     h('span', 'Hello world!'),
     'span node'
   )
 
-  t.same(
+  assert.deepEqual(
     snabby`<div><span>Hello</span><span>world!<span>(You're awesome)</span></span></div>`,
     h('div', [ h('span', 'Hello'), h('span', [ 'world!', h('span', '(You\'re awesome)') ]) ]),
     'nested nodes'
@@ -25,38 +20,26 @@ test('creation', function (t) {
 })
 
 
-test('event listeners', function (t) {
-  t.plan(2)
-
+test('event listeners', function () {
   function sample () {}
   function sample2 () {}
 
-  t.same(
+  assert.deepEqual(
     snabby`<span @on:click=${sample}>Hello world</span>`.data.on,
     { click: sample },
     'single listener'
   )
 
-  t.same(
+  assert.deepEqual(
     snabby`<span @on:click=${sample} @on:input=${sample2}></span>`.data.on,
     { click: sample, input: sample2 },
     'multiple listeners'
   )
-
- // t.same(
- //   snabby`<span :click=${sample} :keydown=${sample2}>`.data.on,
- //   { click: sample, keydown: sample2 },
- //   'shorthand listeners'
- // )
 })
 
 
-test('class attribute', function (t) {
-  t.plan(1)
-
-  console.log(snabby`<span class='foo'></span>`)
-
- t.is(
+test('class attribute', function () {
+  assert.equal(
     snabby`<span class='foo'>Hello world</span>`.data.attrs.class,
     'foo',
     'adds class selector'
@@ -64,54 +47,50 @@ test('class attribute', function (t) {
 })
 
 
-test('boolean attribute', function (t) {
-  t.plan(8)
-
-  t.is(
+test('boolean attribute', function () {
+  assert.equal(
     snabby`<img disabled="false" />`.data.attrs.disabled,
     false,
     'sets false value for boolean attributes'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img disabled="true" />`.data.attrs.disabled,
     'true',
     'sets value for boolean attributes'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img disabled />`.data.attrs.disabled,
     'disabled',
     'sets value for boolean attributes'
   )
 
-
-  t.is(
+  assert.equal(
     snabby`<img @attrs:disabled=${false} />`.data.attrs.disabled,
     false,
     'preserves value for boolean attributes in @attrs: directives'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img @attrs:disabled=${'false'} />`.data.attrs.disabled,
     false,
     'preserves value for boolean attributes in @attrs: directives'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img @attrs:disabled=${'disabled'} />`.data.attrs.disabled,
     'disabled',
     'preserves value for boolean attributes in @attrs: directives'
   )
 
-
-  t.is(
+  assert.equal(
     snabby`<img @attrs:disabled=${true} />`.data.attrs.disabled,
     'true',
     'preserves value for boolean attributes in @attrs: directives'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img @attrs:disabled=${'true'} />`.data.attrs.disabled,
     'true',
     'preserves value for boolean attributes in @attrs: directives'
@@ -119,40 +98,38 @@ test('boolean attribute', function (t) {
 })
 
 
-test('non-boolean attribute', function (t) {
-  t.plan(6)
-
-  t.is(
+test('non-boolean attribute', function () {
+  assert.equal(
     snabby`<img draggable="false" />`.data.attrs.draggable,
     'false',
     'preserves "false" value for non-boolean attributes'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img draggable="true" />`.data.attrs.draggable,
     'true',
     'preserves "true" value for non-boolean attributes'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img @attrs:draggable=${false} />`.data.attrs.draggable,
     'false',
     'preserves value for non-boolean attributes in @attrs: directives'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img @attrs:draggable=${'false'} />`.data.attrs.draggable,
     'false',
     'preserves value for non-boolean attributes in @attrs: directives'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img @attrs:draggable=${true} />`.data.attrs.draggable,
     'true',
     'preserves value for non-boolean attributes in @attrs: directives'
   )
 
-  t.is(
+  assert.equal(
     snabby`<img @attrs:draggable=${'true'} />`.data.attrs.draggable,
     'true',
     'preserves value for non-boolean attributes in @attrs: directives'
@@ -160,52 +137,48 @@ test('non-boolean attribute', function (t) {
 })
 
 
-test('non-string attribute value', function (t) {
-  t.plan(1)
-  t.is(
+test('non-string attribute value', function () {
+  assert.equal(
     snabby`<input @class:x=${false}>`.data.class.x,
     false,
     'sets a prop to the real false value, not a string'
   )
 })
 
-test('props', function (t) {
-  t.plan(4)
 
-  t.is(
+test('props', function () {
+  assert.equal(
     snabby`<input @props:enabled=${false}>`.data.props.enabled,
     false,
     'sets a prop to the real false value, not a string'
   )
 
-  t.is(
+  assert.equal(
     snabby`<input @props:enabled=${true}>`.data.props.enabled,
     'true',
     'sets a prop to the real false value, not a string'
   )
 
-  t.is(
+  assert.equal(
     snabby`<input @props:derp=${'flerp'}>`.data.props.derp,
     'flerp',
     'sets a prop to a string value'
   )
 
-  t.same(
+  assert.deepEqual(
     snabby`<input @props:cool=${ { a: true, b: 345 } }>`.data.props.cool,
     { a: true, b: 345 },
     'sets a prop to an object value'
   )
-
 })
 
 
-test('flatten array children', function (t) {
-  t.plan(1)
+test('flatten array children', function () {
   const arr = [
     snabby`<span>1</span>`,
     snabby`<span>2</span>`,
   ]
-  t.is(
+  assert.equal(
     snabby`<span> ${arr} ${arr} </span>`.children.length,
     7,
     'flattens nested arrays'
@@ -213,9 +186,7 @@ test('flatten array children', function (t) {
 })
 
 
-test('thunk', function (t) {
-  t.plan(4)
-
+test('thunk', function () {
   let invocationCount = 0
 
   const numberView = function (n) {
@@ -223,15 +194,14 @@ test('thunk', function (t) {
     return snabby`<span>Number is ${n}</span>`
   }
 
-  t.same(invocationCount, 0)
+  assert.equal(invocationCount, 0)
 
   let view = snabby.update(document.body, snabby`<div>${snabby.thunk('num', numberView, [1])}</div>`)
-  t.same(invocationCount, 1)
+  assert.equal(invocationCount, 1)
 
   view = snabby.update(view, snabby`<div>${snabby.thunk('num', numberView, [1])}</div>`)
-  t.same(invocationCount, 1)
+  assert.equal(invocationCount, 1)
 
   view = snabby.update(view, snabby`<div>${snabby.thunk('num', numberView, [4])}</div>`)
-  t.same(invocationCount, 2)
-
+  assert.equal(invocationCount, 2)
 })
